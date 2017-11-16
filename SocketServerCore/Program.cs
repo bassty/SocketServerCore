410884f06b3c29de32670446ecdfe2a441413fd2
Sebastian Wagner <sebastianr.wagner@outlook.com>
2017-11-16 11:42:18 +0100

    Projektdateien hinzufügen.

---
+++ SocketServerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
